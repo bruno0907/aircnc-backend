02ca7b9a09afc4f7409683534cd988be5adfd4c9
Bruno Mariani <bruno0907@gmail.com>
2020-07-22 19:53:18 -0300

    Socket.io and approval/rejection implementation

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const socketio = require('socket.io');
+const http = require('http');
+
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
 require('dotenv').config();
 
+
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 const PORT = process.env.PORT || 3333;
 const DB = process.env.DB_URI;
@@ -21,10 +29,27 @@ mongoose.connect(DB, {
 
 
 
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+})
+
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.use(routes);
 
-app.listen(PORT, function() {
+server.listen(PORT, function() {
     console.log(`Server Running on ${this.address().port} port`)
 });
